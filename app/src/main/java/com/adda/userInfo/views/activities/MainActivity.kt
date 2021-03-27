42a511647deb32ac3ce8df43a9947d2e347ee310
kumar.arun2 <arunmehra090>
2021-03-27 21:08:57 +0530

    unused file is removed

--- app/src/main/java/com/adda/userInfo/views/activities/MainActivity.kt
+++ app/src/main/java/com/adda/userInfo/views/activities/MainActivity.kt
@@ -1,24 +1,18 @@
 package com.adda.userInfo.views.activities
 
 import android.os.Bundle
-import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.liveData
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.adda.base.Injection
 import com.adda.databinding.ActivityMainBinding
 import com.adda.extension.gone
 import com.adda.extension.show
 import com.adda.models.ResultState
-import com.adda.remote.RetrofitBuilder
 import com.adda.userInfo.viewmodels.UserInfoViewModel
 import com.adda.userInfo.views.adapters.UserInfoAdapter
 import kotlinx.android.synthetic.main.content_main.view.*
-import kotlinx.coroutines.launch
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
